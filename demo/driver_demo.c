4bf761cb80a975546f57667b1b5a4f8140f61d49
Reda <reda.guendouz777@gmail.com>
2022-03-30 17:32:23 +0200

    c bon

--- demo/driver_demo.c
+++ demo/driver_demo.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <unistd.h>
 
 #define NB_METAS 31
 
@@ -37,33 +36,6 @@ static void print_array (int n, float a[n][n]) {
          printf ("%f\n", a[i][j]);
 }
 
-void swap(double* xp, double* yp)
-{
-    double temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
- 
-// Function to perform Selection Sort
-void selectionSort(double arr[], int n)
-{
-    int i, j, min_idx;
- 
-    // One by one move boundary of unsorted subarray
-    for (i = 0; i < n - 1; i++) {
- 
-        // Find the minimum element in unsorted array
-        min_idx = i;
-        for (j = i + 1; j < n; j++)
-            if (arr[j] < arr[min_idx])
-                min_idx = j;
- 
-        // Swap the found minimum element
-        // with the first element
-        swap(&arr[min_idx], &arr[i]);
-    }
-}
-
 int main (int argc, char *argv[]) {
    
    // Vérification des arguments
@@ -83,13 +55,11 @@ int main (int argc, char *argv[]) {
    // Nombre de repetition de la mesure
    int repm = atoi (argv[3]);
 
-   //Matrice de double61
-   double val[repm][NB_METAS];
-
-   double mediane[repm];
-
-   for (m = 0; m < NB_METAS; m++)
+   for (int indice = 1 ; indice < repw; indice++ )
    {
+
+      for (m=0; m<NB_METAS; m++) {
+         
          // Allocation pour les 2 tableaux et la matrice
          float (*x) = malloc (size * sizeof x[0]);
          float (*y) = malloc (size * sizeof y[0]);
@@ -101,31 +71,30 @@ int main (int argc, char *argv[]) {
          init_array (size, y);
          init_matrice(size,z);
 
-         // Mesure des repetitions
-         for (i=0; i<repm; i++){
-            uint64_t t1 = rdtsc();
-	    baseline (size, x, y, z);
-            uint64_t t2 = rdtsc();
-
-            val[i][m] = (double)(t2 -t1);
-
-            // Affichage des performances
-            //printf ("%.2f\n",(double)(t2 - t1));
+         // Warmup
+         if (m == 0) {
+            for (i=0; i<indice; i++){
+               baseline (size, x, y, z);
+            }
+         } else {
+            baseline (size, x, y, z);
          }
-         sleep(2);
+
+         // Mesure des repetitions
+         uint64_t t1 = rdtsc();
+         for (i=0; i<repm; i++){
+            baseline (size, x, y, z);
+         }   
+         uint64_t t2 = rdtsc();
+
+         // Affichage des performances
+         printf ("%.2f cycles/FMA\n",(t2 - t1) / ((float) size * size * size * repm));
+
          // Libere l'espace memoire
          free (x);
          free (y);
          free (z);
-   }
-   for (int i = 0; i < repm; i++)
-   {
-      selectionSort(val[i], NB_METAS);
-      mediane[i] = val[i][15];
-      printf("%.2f\n",mediane[i]);
+      }
    }
    return EXIT_SUCCESS;
 }
-
-
-
