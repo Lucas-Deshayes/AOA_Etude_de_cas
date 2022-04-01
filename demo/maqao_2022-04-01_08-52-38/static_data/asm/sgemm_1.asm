da9e98abc3b506218cbc611bc706f29159dbb48a
root <root@Reda-PC.localdomain>
2022-04-01 08:57:36 +0200

    icc O2

---
+++ demo/maqao_2022-04-01_08-52-38/static_data/asm/sgemm_1.asm
@@ -0,0 +1,9 @@
+address;source_location;insn;indent
+0x4014e2;:0;MOV	%R12,%RSI;
+0x4014e5;:0;MOV	%R14,%RDX;
+0x4014e8;:0;MOV	%R13,%RCX;
+0x4014eb;:0;MOV	0x58(%RSP),%EDI;
+0x4014ef;:0;CALL	4015b0 <baseline>;
+0x4014f4;:0;INC	%EBX;
+0x4014f6;:0;CMP	%R15D,%EBX;
+0x4014f9;:0;JL	4014e2 <main+0x252>;
---
+++ demo/maqao_2022-04-01_08-52-38/static_data/hierarchy/fct_sgemm_7.lua
@@ -0,0 +1,14 @@
+__fct = {
+  loops = {
+    _7 = {
+      src = "",
+      src_regions = {
+      },
+      _8 = {
+        src = "",
+        src_regions = {
+        },
+      },
+    },
+  },
+}
